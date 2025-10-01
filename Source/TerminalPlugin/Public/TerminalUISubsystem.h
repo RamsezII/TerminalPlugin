f30d4fec55ccc3e0a38c37a8e6f6f353b2bcf132
ram16 <thomass.montaine@gmail.com>
2025-10-02 00:54:22 +0200

    continuer par code

---
+++ Source/TerminalPlugin/Public/TerminalUISubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/LocalPlayerSubsystem.h"
+#include "TerminalUISubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TERMINALPLUGIN_API UTerminalUISubsystem : public ULocalPlayerSubsystem
+{
+	GENERATED_BODY()
+	
+};
